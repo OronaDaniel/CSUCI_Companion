5844a00b60f0d0567a494511b9f96f3d5d0a7f12
tildahh <sara.orona761@myci.csuci.edu>
2023-11-23 02:39:03 -0800

    Removed document functionality

--- app.py
+++ app.py
@@ -3,27 +3,18 @@ import time
 from openai import OpenAI
 from secret import api_key  # Importing API key from secret.py
 from flask_session import Session  # Import session management
-import json
-
-assistant_id = "asst_hTHe8cgWzKJnrYF8Si0OswnZ"
 
 client = OpenAI(api_key=api_key)
 
-def show_json(obj):
-    print(json.dumps(json.loads(obj.model_dump_json()), indent=4))
-
-# Pretty printing helper
-def pretty_print(messages):
-    print("# Messages")
-    for m in messages:
-        print(f"{m.role}: {m.content[0].text.value}")
-    print()
-
+# Flask app setup
 app = Flask(__name__)
 
 # Configure the session type
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+# Your existing assistant ID
+assistant_id = "asst_hTHe8cgWzKJnrYF8Si0OswnZ"
 
 @app.route('/')
 def index():
@@ -42,37 +33,41 @@ def send_message():
     if thread_id is None:
         return jsonify({'error': 'Session expired or invalid.'}), 400
 
-    # add message to thread from user input
-    message = client.beta.threads.messages.create(thread_id=thread_id, role="user", content=user_input)
-    show_json(message)
+    # Add a Message to a Thread
+    message = client.beta.threads.messages.create(
+        thread_id=thread_id,
+        role="user",
+        content=user_input
+    )
 
-    # create the asynchronous run
+    # Run the Assistant using the existing assistant ID
     run = client.beta.threads.runs.create(
         thread_id=thread_id,
-        assistant_id=assistant_id,
+        assistant_id=assistant_id
     )
-    show_json(run)
 
-    # sleep every 0.5s to check status
+    # Wait for a response from the assistant
     while True:
-        time.sleep(0.5)
+        time.sleep(4)
 
-        # retrieve status
-        run_status = client.beta.threads.runs.retrieve(thread_id=thread_id, run_id=run.id)
-        show_json(run)
+        # Retrieve the run status
+        run_status = client.beta.threads.runs.retrieve(
+            thread_id=thread_id,
+            run_id=run.id
+        )
 
-        # if status is completed 
+        # If run is completed, get messages
         if run_status.status == 'completed':
-            messages = client.beta.threads.messages.list(thread_id=thread_id, order="asc", after=message.id)
-            show_json(messages)
-            pretty_print(messages)
+            messages = client.beta.threads.messages.list(
+                thread_id=thread_id
+            )
 
             # Find the assistant's response and return it
             for msg in messages.data:
                 if msg.role == 'assistant':
                     content = msg.content[0].text.value
                     return jsonify({'reply': content})
-
+                
 @app.route('/reset_conversation', methods=['POST'])
 def reset_conversation():
     # Clear any existing session data
@@ -83,7 +78,7 @@ def reset_conversation():
     session['thread_id'] = thread.id
 
     # Return a confirmation message
-    return jsonify({'message': 'Conversation has been reset.'})
+    return jsonify({'message': 'Conversation has been reset.'})                
 
 if __name__ == '__main__':
     app.run(debug=True)
